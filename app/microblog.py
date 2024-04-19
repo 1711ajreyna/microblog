bd7f469045f2106595ff329dae035c0362a1f2dd
Andrew Reyna <1711ajreyna@gmail.com>
2024-04-18 18:02:38 -0700

    microblog.py correction and migrations

--- app/microblog.py
+++ app/microblog.py
@@ -1,7 +1,9 @@
-from flask import Flask
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Post
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "hello, world!"
+@app.shell_context_processor
+def make_shell_context():
+    return{'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
