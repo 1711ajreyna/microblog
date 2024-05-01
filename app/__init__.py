eabd3479cbf1384f4e86faf24ca652c632b76d87
Andrew Reyna <1711ajreyna@gmail.com>
2024-04-30 17:05:53 -0700

    followers

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate 
-from flask_login import LoginManager
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+from flask_login import LoginManager
+from config import Config
 
 
 app = Flask(__name__)
@@ -20,7 +20,7 @@ if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            AUTH = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
             secure = ()
@@ -34,9 +34,9 @@ if not app.debug:
 
             if not os.path.exists('logs'):
                 os.mkdir('logs')
-            file_handler = RotatingFileHandler('log/microblog.log', maxBytes=10240, backupCount=10)
+            file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
             file_handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(Logging.INFO)
+            file_handler.setLevel(logging.INFO)
             app.logger.addHandler(file_handler)
 
             app.logger.setLevel(logging.INFO)
--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ import sqlalchemy as sa
 from app import app, db
 from app.models import User
 from datetime import datetime, timezone
+from app.forms import EmptyForm
 
 
 @app.before_request
@@ -77,7 +78,8 @@ def user(username):
         {'author': user, 'body':'Test post #1'},
         {'author': user, 'body':'Test post #2'}
     ]
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm
+    return render_template('user.html', user=user, posts=posts, form=form)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
@@ -94,3 +96,43 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+@app.route('/follow/<username>', method=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == username))
+        if user is None:
+            flash(f'User {username} not found.')
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash(f'You are following {username}!')
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+    
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = db.scalar(
+            sa.select(User).where(User.username == username))
+        if user is None:
+            flash(f'User {username} not found.')
+            return  redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollwo(user)
+        db.session.commit()
+        flash(f'You are not following {username}.')
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
